50e1b0a0b06051e0846c2cef7e8da8d6783c4b71
Thiago <thiago_murakami@hotmail.com>
2020-12-23 20:43:58 -0300

    redirecting URL

--- back/public/script.js
+++ back/public/script.js
@@ -4,15 +4,8 @@ const cards = document.querySelectorAll('.card')
 for (let card of cards) {
     card.addEventListener("click", function() {
         const videoId = card.getAttribute("id");
-        modalOverlay.classList.add('active')
-        // take the video
-        modalOverlay.querySelector("iframe").src = `https://www.youtube.com/embed/${videoId}`
+        
+        window.location.href = `/video?id=${videoId}`
+        
     })
 }
-
-// close the windows
-document.querySelector(".close-modal").addEventListener("click", function() {
-    modalOverlay.classList.remove("active")
-    // close de video
-    modalOverlay.querySelector("iframe").src = ""
-})
